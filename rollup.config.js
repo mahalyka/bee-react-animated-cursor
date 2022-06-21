7f9025db7bdf6ddc20117a27a97a698f3b747107
mahalyka <mahalyka@gmail.com>
2022-06-21 18:49:21 +0700

    init source

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import resolve from '@rollup/plugin-node-resolve'
+import pkg from './package.json'
+
+const umdGlobals = {
+  react: 'React',
+  'react-animated-cursor': 'AnimatedCursor'
+}
+
+const getBabelOptions = () => ({
+  exclude: '**/node_modules/**'
+})
+
+export default {
+  input: 'lib/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    },
+    {
+      file: pkg.browser,
+      format: 'umd',
+      name: 'AnimatedCursor',
+      globals: umdGlobals
+    }
+  ],
+  plugins: [
+    external(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs()
+  ]
+}
